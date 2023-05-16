fa9778885f3cc6daff53a97bb7540be421aa22cc
Soyeong <starsuzi@kaist.ac.kr>
2023-05-17 01:20:55 +0900

    add

--- run/gpu2.sh
+++ run/gpu2.sh
@@ -1,3 +1,3 @@
-bash /data/syjeong/prompt_test/run/noTrainer/peft/context/test_time_tuning/squad/xl/run_squad_mc10_epoch1.sh
-bash /data/syjeong/prompt_test/run/noTrainer/peft/context/baseline/squad/xl/run_squad_baseline.sh
-bash /data/syjeong/prompt_test/run/noTrainer/peft/context/train/squad/xl/run_squad.sh
+bash run/noTrainer/peft/context/test_time_tuning/squad/xl/run_squad_mc10_epoch1.sh
+bash run/noTrainer/peft/context/baseline/squad/xl/run_squad_baseline.sh
+bash run/noTrainer/peft/context/train/squad/xl/run_squad.sh
