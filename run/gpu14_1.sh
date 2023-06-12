6f44259aa0fd8b81258c775803926917337a871e
Soyeong <starsuzi@kaist.ac.kr>
2023-06-13 01:59:09 +0900

    add

--- run/gpu14_1.sh
+++ run/gpu14_1.sh
@@ -1,4 +1,4 @@
 #bash /data/syjeong/prompt_test/run/noTrainer/peft/context/test_time_tuning/mmlu/base/subcategory/run_mmlu_hard.sh
 #bash /data/syjeong/prompt_test/run/noTrainer/peft/context/test_time_tuning/mmlu/base/subcategory/run_mmlu_soft.sh
-
+bash /data/syjeong/prompt_test/run/noTrainer/peft/context/test_time_tuning/sciq/xl/do_filtering/instance/run_sciq_instance_nofilter_gpu14.sh
 bash /data/soyeong/prompt_test/run/noTrainer/peft/context/train/trivia/xl/run_trivia_train.sh
---
+++ run/noTrainer/peft/context/test_time_tuning/sciq/xl/do_filtering/instance/run_sciq_instance_nofilter_gpu14.sh
@@ -0,0 +1,39 @@
+DATE=$(date +%Y_%m_%d)/$(date +%H_%M_%S)
+MODEL=google/flan-t5-xl
+DATASET_NAME=sciq
+
+for MC_DROP_NUM in 10 15 # 5 7 10 15
+do
+    for EPOCH in 2 5
+    do
+        for FILTER_THRES in 0.3 0.5 0.7 0.9
+        do
+            OUTPUT_DIR=./outputs/${DATASET_NAME}/context/test_time_tuning/model/${MODEL}/filter_thres/${FILTER_THRES}/orig_prompt/lora/mc/${MC_DROP_NUM}/epoch/${EPOCH}/${DATE}
+            mkdir -p ${OUTPUT_DIR}
+
+            CUDA_VISIBLE_DEVICES=1 python run_squad.py \
+                --filter_thres ${FILTER_THRES} \
+                --model_name_or_path ${MODEL} \
+                --question_column question \
+                --answer_column correct_answer \
+                --context_column support \
+                --learning_rate 3e-5 \
+                --max_seq_length 384 \
+                --doc_stride 128 \
+                --per_device_eval_batch_size 8 \
+                --output_dir ${OUTPUT_DIR} \
+                --overwrite_cache \
+                --train_peft_model \
+                --val_column 'test' \
+                --do_eval \
+                --do_test_time_tuning \
+                --mc_drop_num ${MC_DROP_NUM} \
+                --test_time_tuning_epoch ${EPOCH}
+        done
+    done
+done
+
+            #--num_beams 1 \
+            # --max_train_samples 5 \
+            # --max_eval_samples 5 \
+            # --max_test_time_tuning_samples 5
