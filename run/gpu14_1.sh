2894b4d988b561bf643d2f2d8468685889370e02
Soyeong <starsuzi@kaist.ac.kr>
2023-06-13 01:52:00 +0900

    add

---
+++ run/gpu14_1.sh
@@ -0,0 +1,4 @@
+#bash /data/syjeong/prompt_test/run/noTrainer/peft/context/test_time_tuning/mmlu/base/subcategory/run_mmlu_hard.sh
+#bash /data/syjeong/prompt_test/run/noTrainer/peft/context/test_time_tuning/mmlu/base/subcategory/run_mmlu_soft.sh
+
+bash /data/soyeong/prompt_test/run/noTrainer/peft/context/train/trivia/xl/run_trivia_train.sh
